ce1f754c24e8311d32a095937f5724093077fb61
Alohadancemeow <nawongsak@gmail.com>
2023-06-05 15:06:04 +0700

    🍀 set up supabase provider, auth, context

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React, { useState } from "react";
+
+import { Database } from "../types/supabase";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const SupabaseProvider = ({ children }: Props) => {
+  const [supabaseClient] = useState(() =>
+    createClientComponentClient<Database>()
+  );
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
+};
+
+export default SupabaseProvider;
